6946bab2fef98f0ba0abbd6448def5cd75f036fb
AcePizza <stianchrbakke@gmail.com>
2022-06-29 13:46:07 +0200

    added controller function

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
-let url =
-  "https://gateway.marvel.com/v1/public/characters?ts=1&apikey=dd7f4f144fe3786105bea06c9a02d3ee&hash=8fda5d5a302ba2bbf1db8ceb456f928f";
+// Second project - WebDev
 
-async function getMarvelData(str) {
-  const response = await fetch(str); //The first promise (to check if the data is avilible)
-  let marData = await response.json(); //The second promise (to put the data into a usable format(JSON))
-  mainStuffTest(marData); //* This is important. It calls the function below with the data from this function
+async function getMarvelData() {
+  try {
+    const response = await fetch(
+      "https://gateway.marvel.com/v1/public/characters?ts=1&apikey=dd7f4f144fe3786105bea06c9a02d3ee&hash=8fda5d5a302ba2bbf1db8ceb456f928f"
+    );
+    let marvelData = await response.json();
+    return marvelData.data.results;
+  } catch (error) {
+    console.log("Error", error);
+  }
 }
 
-getMarvelData(url); //You need to call the fuction with the url variable from the top of the code
-
-function navBarTop() {
-  
-}
-
-function mainStuffTest(marvelData) {
-  for (let i = 0; i < marvelData.data.results.length; i++) {
+function displayCardData(marvelData) {
+  for (let i = 0; i < marvelData.length; i++) {
     let divMainStuff = document.getElementById("main-Stuff");
     let divCard = document.createElement("div");
     divCard.setAttribute("class", "col-sm-12 col-md-6 col-lg-4");
@@ -22,10 +21,10 @@ function mainStuffTest(marvelData) {
     let img = document.createElement("img");
     img.setAttribute(
       "src",
-      marvelData.data.results[i].thumbnail.path +
+      marvelData[i].thumbnail.path +
         "/portrait_medium" +
         "." +
-        marvelData.data.results[i].thumbnail.extension
+        marvelData[i].thumbnail.extension
     );
     img.setAttribute("alt", "Thumbnail");
     img.classList.add("card-img-top");
@@ -35,7 +34,7 @@ function mainStuffTest(marvelData) {
 
     let h5 = document.createElement("h5");
     h5.classList.add("card-title");
-    h5.innerHTML = marvelData.data.results[i].name;
+    h5.innerHTML = marvelData[i].name;
 
     let collapsDiv = document.createElement("div");
     collapsDiv.setAttribute("class", "collapse");
@@ -58,17 +57,24 @@ function mainStuffTest(marvelData) {
 
     let divText = document.createElement("div");
     divText.setAttribute("class", "card card-body");
-    if (marvelData.data.results[i].description == "") {
+    if (marvelData[i].description == "") {
       divText.classList.add("font-italic");
       divText.classList.add("text-muted");
       divText.innerHTML = "No description available";
       cardBody.appendChild(divText);
       // collapsDiv.appendChild(divText);
     } else {
-      divText.innerHTML = marvelData.data.results[i].description;
+      divText.innerHTML = marvelData[i].description;
       cardBody.appendChild(collapsDiv);
       collapsDiv.appendChild(divText);
       divCard.appendChild(btnCollExp);
     }
   }
+}
+
+async function controller() {
+  // Run first stuff
+  const marvelData = [await getMarvelData()];
+  // Run second stuff
+  displayCardData(marvelData);
 }
